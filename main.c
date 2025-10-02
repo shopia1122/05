be29c175f8d8f3216e1a7e165d27cd429bfec95e
shopia1122 <shopia123@sookmyung.ac.kr>
2025-10-02 16:14:42 +0900

    main6

--- main.c
+++ main.c
@@ -3,41 +3,32 @@
 
 int main(int argc, char *argv[])
 {
-    int first;
-    int second;
-    char cal;
-    int result;
+    int answer = 59;
+    int num;
+    int trial=0;
     
-    printf("enter the calculation: ");  
-    scanf("%d %c %d", &first, &cal, &second);
+    do
+    {
+    printf("Guess a number: ");  
+    scanf("%d", &num);
     
-    if(cal == '+')   
-      result = first + second;
-    else if(cal == '-')
-      result = first - second;
-    else if(cal == '*')
-      result = first * second;
-    else if(cal == '/')
-      result = first / second;
-    else 
-      printf("Wrong Operation.\n");
+    if(num < answer)
+      printf("low!\n");
+    else if(num > answer)
+      printf("high!\n");
+      
+    trial = trial +1;     //loop가 돌 때마다 trial도 돌게 되어서 시도 횟수를 출력하게 되는 것.  
+    }
+    while(answer != num);  // answer과 num이 같지 않아야 참인 것이고 그래야 루프가 정답 찾을 때까지 계속 반복하게 됨. 나는 그냥 같다고 두었었음.  
     
-    printf(" = %d\n", result); 
+    
+    printf("Congratulation! trials: %d\n", trial);
+    
+    
+    
   
   
-  /*  switch문으로 하는 경우 이용하면 되는 코드 
-  switch(op)
-  {
-            case '+':              작은 따옴표가 있어야 된다. 
-                 result = a+b;
-                 break;
-            case:
-                 defalut:  
-            }
-  
-  
-  */
-  
+ 
   
   
   system("PAUSE");	
